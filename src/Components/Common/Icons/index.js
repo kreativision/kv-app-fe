b404dff119a49e2c550401e4b0925a256d1601d2
amittras-pal <pal.amittras@gmail.com>
2021-10-16 18:21:02 +0530

    FontAwesome Library Component

---
+++ src/Components/Common/Icons/index.js
@@ -0,0 +1,9 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faCheckSquare,
+  faCoffee,
+  faUser,
+  faLock,
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(faCheckSquare, faCoffee, faUser, faLock);
